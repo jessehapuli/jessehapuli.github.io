84a11eaa20facc4dcabe217ce369e3bbfca9a2d3
jessehapuli <Jessekvh@gmail.com>
2024-04-10 12:40:03 +0300

    no

--- .history/CV/index_20240410105335.js
+++ .history/CV/index_20240410105335.js
@@ -1,6 +1,5 @@
 function myFunction(x) {
   x.classList.toggle("change");
-}
 
 function myFunction(y) {
   var y = document.getElementById("ge");
@@ -8,6 +7,9 @@ function myFunction(y) {
     y.style.display = "none";
   } else {
     y.style.display = "block";
-  }
-  if (x.classList.toggle("change") = toggle("ge"))
+  }}
+
+if (x.classList.toggle("change") = y); 
+
 }
+
