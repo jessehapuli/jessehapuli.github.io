95290712f96ce1229ed6455f8694be2f92b366a3
jessehapuli <Jessekvh@gmail.com>
2024-04-10 08:52:23 +0300

    added new project rocket

---
+++ .history/index_20240409141356.js
@@ -0,0 +1,11 @@
+function myFunction(x) {
+  x.classList.toggle("change");
+
+
+  var x = document.getElementById("myLinks");
+  if (x.style.display === "block") {
+    x.style.display = "none";
+  } else {
+    x.style.display = "block";
+  }
+}
