40d314d668bfe252bc27e72e41dbc67ab879bb3c
jessehapuli <Jessekvh@gmail.com>
2024-04-11 11:06:11 +0300

    10

---
+++ .history/CV/index_potential_20240411103906.js
@@ -0,0 +1,12 @@
+function myFunction(x) {
+  x.classList.toggle("change"); y.classList.toggle("hidden");
+}
+
+function myFunction() {
+  var x = document.getElementById("myLinks");
+  if (x.style.display === "block") {
+    x.style.display = "none";
+  } else {
+    x.style.display = "block";
+  }
+}
