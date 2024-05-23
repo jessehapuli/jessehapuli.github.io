2314763da9e4c0a2a0fd525b1566b2f6c45ee057
jessehapuli <Jessekvh@gmail.com>
2024-05-23 13:24:42 +0300

    jes

--- .history/muut projektit/rocket/alertti_20240411111903.js
+++
@@ -1,8 +0,0 @@
-var number1 = 10;
-var number2 = 100;
-
-if (number1 > number2) {
-    alert('Syntax error');
-};
-
-console.log('hello World');
--- .history/muut projektit/rocket/alertti_20240411124747.js
+++
@@ -1,8 +0,0 @@
-var Jesse = 10;
-var Hapuli = 100;
-
-if (Jesse > Hapuli) {
-  true; alert('Jesse');
-} else {
-    false; alert('Hapuli');
-}
