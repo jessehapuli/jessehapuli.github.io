b198bfd7f5cb94956c3a7cd284035c687b3a6a0f
jessehapuli <Jessekvh@gmail.com>
2024-05-23 11:05:26 +0300

    delete history

--- .history/snake/snake_20240507111525.js
+++
@@ -1,7 +0,0 @@
-var canvas = document.getcontext('2d');
-var game = document.getElementById('game');
-
-var apple = document.getElementById('apple')
-var snaketail = document.getElementById('snaketail')
-var snakehead = document.getElementById('snakehead')
-
